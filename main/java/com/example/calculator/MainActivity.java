0c632dfe383671f2f802b5a53df03535a137affa
ilhamaf22 <61315182+ilhamaf22@users.noreply.github.com>
2021-11-11 16:01:55 +0700

    src file

---
+++ main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView workingsTV;
+    TextView resultsTV;
+
+    String workings = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initTextViews();
+    }
+
+    private void initTextViews() {
+        workingsTV = (TextView)findViewById(R.id.workingsTextView);
+        resultsTV = (TextView)findViewById(R.id.resultTextView);
+    }
+    private void setWorkings(String givenValue) {
+        workings = workings + givenValue;
+        workingsTV.setText(workings);
+    }
+    public void clearOnClick(View view) {
+        setWorkings("");
+        workings = "";
+        resultsTV.setText("");
+        leftBracket = true;
+    }
+
+    boolean leftBracket = true;
+    public void bracketOnClick(View view) {
+        if(leftBracket) {
+            setWorkings("(");
+            leftBracket = false;
+        } else {
+            setWorkings(")");
+            leftBracket = true;
+        }
+
+    }
+
+    public void resultOnClick(View view) {
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        try {
+            result = (double) engine.eval(workings);
+        } catch (ScriptException e)
+        {
+            Toast.makeText(this,"invalid Input", Toast.LENGTH_SHORT).show();
+        }
+        if(result != null)
+            resultsTV.setText(String.valueOf(result.doubleValue()));
+    }
+
+    public void percentOnClick(View view) {
+        setWorkings("/100");
+    }
+
+    public void divideOnClick(View view) {
+        setWorkings("/");
+    }
+
+    public void SevenOnClick(View view) {
+        setWorkings("7");
+    }
+
+    public void eightOnClick(View view) {
+        setWorkings("8");
+    }
+
+    public void nineOnClick(View view) {
+        setWorkings("9");
+    }
+
+    public void multiplyOnClick(View view) {
+        setWorkings("*");
+    }
+
+    public void fourOnClick(View view) {
+        setWorkings("4");
+    }
+
+    public void fiveOnClick(View view) {
+        setWorkings("5");
+    }
+
+    public void sixOnClick(View view) {
+        setWorkings("6");
+    }
+
+    public void minusOnClick(View view) {
+        setWorkings("-");
+    }
+
+    public void oneOnClick(View view) {
+        setWorkings("1");
+    }
+
+    public void twoOnClick(View view) {
+        setWorkings("2");
+    }
+
+    public void threeOnClick(View view) {
+        setWorkings("3");
+    }
+
+    public void plusOnClick(View view) {
+        setWorkings("+");
+    }
+
+    public void decimalOnClick(View view) {
+        setWorkings(".");
+    }
+
+    public void zeroOnClick(View view) {
+        setWorkings("0");
+    }
+
+
+}
